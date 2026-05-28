b456da9c9b007da06e998086dc64b7cfb03ed38a
Keita Kobayashi <keita@kbys.me>
2026-05-29 07:08:32 +0900

    cargo fmt

--- crates/parser/src/parse.rs
+++ crates/parser/src/parse.rs
@@ -603,10 +603,9 @@ impl<'a> StreamParser<'a> {
                     surface.active_boundary = Some(BoundaryKind::Interior(interior_index));
                     surface.boundary_depth = Some(depth);
                 }
-                b"GM_Ring"
-                    if surface.active_boundary.is_some() => {
-                        surface.ring_depth = Some(depth);
-                    }
+                b"GM_Ring" if surface.active_boundary.is_some() => {
+                    surface.ring_depth = Some(depth);
+                }
                 _ => {}
             }
 
